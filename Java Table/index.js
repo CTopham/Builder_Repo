65455b7390b7b02f3e6c42c8e8251c74108c6e12
Craig <35710823+CTopham@users.noreply.github.com>
2018-05-19 09:45:44 -0400

    Adding

---
+++ Java Table/index.js
@@ -0,0 +1,49 @@
+// Get references to the tbody element, input field and button
+var $tbody = document.querySelector("tbody");
+var $stateInput = document.querySelector("#state");
+var $searchBtn = document.querySelector("#search");
+
+//    Set up for button function
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+// Set
+var filteredAlien = alienData;
+
+// renderTable renders the filteredAlien to the tbody
+function renderTable() {
+    $tbody.innerHTML = "";
+    for (var i = 0; i < filteredAlien.length; i++) {
+      // Get get the current address object and its fields
+      var spotting = filteredAlien[i];
+      var fields = Object.keys(spotting);
+      // Create a new row in the tbody, set the index to be i + startingIndex
+      var $row = $tbody.insertRow(i);
+      for (var j = 0; j < fields.length; j++) {
+        // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+        var field = fields[j];
+        var $cell = $row.insertCell(j);
+        $cell.innerText = spotting[field];
+      }
+    }
+  }
+
+// ========================== Search Function =========================================
+
+function handleSearchButtonClick() {
+  // Format the user's search by removing leading and trailing whitespace, lowercase the string
+  var filterState = $stateInput.value.trim().toLowerCase();
+
+  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
+  filteredAddresses = alienData.filter(function(spotting) {
+    var addressState = spotting.state.toLowerCase();
+
+    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+    return addressState === filterState;
+  });
+  renderTable();
+}
+
+// Render the table for the first time on page load
+renderTable();
+
+
