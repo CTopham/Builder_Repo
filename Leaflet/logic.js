9b82b65a47a00e04630678999816267587a56893
Craig <35710823+CTopham@users.noreply.github.com>
2018-07-01 17:09:02 -0400

    Updating

--- Leaflet/logic.js
+++ Leaflet/logic.js
@@ -1,83 +1,13 @@
-// Mapbox API
-var mapbox = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3RvcGhhbSIsImEiOiJjamh4aHJ4amgwYnhiM3FtcTNrNnhyMHZ3In0.A5V1_2ns65IgvuxqNHvDgQ";
-
-// Creating map object
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
 var myMap = L.map("map", {
-  center: [40.7, -73.95],
-  zoom: 1.3
+  center: [45.52, -122.67],
+  zoom: 5
 });
 
-// Adding tile layer to the map
-L.tileLayer(mapbox).addTo(myMap);
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/ctopham/cjis5dcbs1rre2so1wae9r4ei/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3RvcGhhbSIsImEiOiJjamh4aHJ4amgwYnhiM3FtcTNrNnhyMHZ3In0.A5V1_2ns65IgvuxqNHvDgQ").addTo(myMap);
 
 
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-
-
-// Grabbing the data with d3..
-d3.json(url, function(response) {
-
-    // Creating a new marker cluster group
-    var markers = L.markerClusterGroup();
-    console.log(response.features)
-
-  // Loop through our data...
-    for (var i = 0; i < response.features.length; i++) {
-      // set the data location property to a variable
-      console.log(response.features[i].properties.mag)
-      var location = response.features[i].geometry;
-  
-      // If the data has a location property...
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.circle([location.coordinates[1], location.coordinates[0]],{fillOpacity: 0.75,
-          color: "red",
-          fillColor: "purple", 
-          radius:response.features[i].properties.mag}));
-
-      }
-      
-    }
-    
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-});
-
-
-for (var i = 0; i < response.features.length; i++) {
-  L.circle([location.coordinates[1], location.coordinates[0]], {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: "purple",
-    // Setting our circle's radius equal to the output of our markerSize function
-    // This will make our marker's size proportionate to its population
-    radius: markerSize(response.features[i].properties.mag*400)
-  }).bindPopup("<h1>" + response.features[i].properties.mag + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
-}
-  //===============================Bubbles=================================================
-  
-  // //var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-  // d3.json(url, function(response) {
-  
-  // console.log(response.features[2].geometry.coordinates)
-  // console.log(response.features[1].properties.mag)  
-    
-  //   function markerSize(mag) {
-  //   return response.features[1].properties.mag * 10;
-  // }
-  
-  // // Loop through the cities array and create one marker for each city object
-  // for (var i = 0; i <response.features.length; i++) {
-  //   var bubble = response.features[i].geometry;
-  //   L.circle(bubble.coordinates[1], bubble.coordinates[0], {
-  //     fillOpacity: 0.75,
-  //     color: "white",
-  //     fillColor: "purple",
-  //     // Setting our circle's radius equal to the output of our markerSize function
-  //     // This will make our marker's size proportionate to its population
-  //     radius: markerSize(response.features[i].properties.mag)
-  //   }).bindPopup("<h1>" + response.features[i].properties.place + "</h1> <hr> <h3> Address: " + response.features[i].properties.place + "</h3>").addTo(myMap);
-  
-  
-  // } });
--- Leaflet/logic4.js
+++ Leaflet/logic4.js
@@ -3,48 +3,41 @@ function markerSize(earthquake_size) {
     return Math.pow((earthquake_size *100.5),2);
   }
   
-  function createMap(magnitudes, earthquakes) {
+function createMap(magnitudes, earthquakes) {
   
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
       "access_token=pk.eyJ1IjoiY2thcm5hcyIsImEiOiJjamh4aHJzcXgwYndpM3dydmV6aHNtNXdqIn0.LkWE7jBeB8nZmUqZNLgLvg");
   
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoiY2thcm5hcyIsImEiOiJjamh4aHJzcXgwYndpM3dydmV6aHNtNXdqIn0.LkWE7jBeB8nZmUqZNLgLvg");
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/ctopham/cjis5dcbs1rre2so1wae9r4ei/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3RvcGhhbSIsImEiOiJjamh4aHJ4amgwYnhiM3FtcTNrNnhyMHZ3In0.A5V1_2ns65IgvuxqNHvDgQ");
   
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
       "Street Map": streetmap,
-      "Dark Map": darkmap
+      "Plate Boundaries": darkmap
     };
+  
   
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
       Magnitudes: magnitudes,
-      Earthquakes: earthquakes
+      Earthquakes: earthquakes,
     };
     console.log("++++++++++++++++")
     console.log(overlayMaps.Earthquakes)
     console.log(overlayMaps.Magnitudes)
+    
     console.log("++++++++++++++")
+
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
       center: [
-        37.09, -95.71
+        37.09, -15.71
       ],
-      zoom: 3,
+      zoom: 2,
       layers: [streetmap, magnitudes,earthquakes]
     });
-  
-  
-  
-  
-    //Another Test...this plots the magnitude
-    // sizeMarkers.forEach(function(element) {
-    //   console.log(element);
-    //   element.addTo(myMap);
-    // });
-  //Another Test
+    
   
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
@@ -69,46 +62,35 @@ function markerSize(earthquake_size) {
         two_coords.push(feature.geometry.coordinates[0]); 
         console.log(feature.properties.mag);
   
-        
-  
-        sizeMarkers.push(
+          sizeMarkers.push(
           L.circle(two_coords, {
             color: 'red',
             stroke: false,
             fillColor: '#f03',
-            fillOpacity: (feature.properties.mag/5),
+            fillOpacity: (.20),
             radius: markerSize(feature.properties.mag)
           })
         );
     }
-    console.log("All together")
-    console.log(sizeMarkers);
+
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
       onEachFeature: onEachFeature
     });
     var magnitudes = L.layerGroup(sizeMarkers);
-    console.log("let's compare")
-    console.log(magnitudes)
+  
     // Sending our earthquakes layer to the createMap function
     createMap(magnitudes,earthquakes);
     //console.log(sizeMarkers)
-  
-  
-  
-  }
-  
-  
-  
-  
+}
   // Store our API endpoint inside queryUrl
-  //var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  //  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-  var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
-  //var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+  var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
   // Perform a GET request to the query URL
   d3.json(queryUrl, function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
+
     createFeatures(data.features);
+    
   });
